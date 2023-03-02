1cd3a614d2439a5364790041df49aa77a36ea3f0
Karberg <jeppehkarberg@gmail.com>
2023-03-02 23:08:11 +0100

    Initial Main Menu

--- My project/Assets/Scripts/Enemy.cs
+++ My project/Assets/Scripts/Enemy.cs
@@ -7,7 +7,6 @@ public class Enemy : MonoBehaviour
     public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
-    float nextAnimation = 0f;
     public float animationRate = 2f;
     
     // Start is called before the first frame update
